bba63b3c40faa95b67fec3dfe6b01ffdd3184888
ShantoSarkar34 <shanto9070sarkar@gmail.com>
2023-12-27 13:40:18 +0600

    hero section fix

--- app/components/Hero.jsx
+++ app/components/Hero.jsx
@@ -30,9 +30,9 @@ function Hero() {
                 Suspendisse ac convallis sapien, quis commodo libero. Donec nec
                 duomoi luctus, pellentesque lacus sed, mollis going leo.
               </p>
-              <div className="pt-5">
+              <div className="pt-5 z-40">
                 <Link href={"#"} className="flex items-center">
-                  <div className="">
+                  <div className=" z-30 animate-spining">
                     <svg
                       width={60}
                       fill="#027EEC"
@@ -43,7 +43,7 @@ function Hero() {
                       <path d="M1.51038 32.6729C-0.990382 34.1837 -0.232333 37.9945 2.65612 38.4335C4.97848 38.7866 6.09439 41.4804 4.70164 43.3722C2.96938 45.7248 5.12803 48.9555 7.9646 48.2555C10.245 47.6925 12.3074 49.7548 11.7445 52.0354C11.0445 54.8721 14.2752 57.0306 16.6278 55.2984C18.5196 53.9057 21.2139 55.0216 21.5665 57.3439C22.0054 60.2322 25.8161 60.9904 27.3271 58.4896C28.5419 56.479 31.4581 56.479 32.6729 58.4896C34.1837 60.9904 37.9945 60.2323 38.4335 57.3439C38.7866 55.0215 41.4804 53.9056 43.3722 55.2984C45.7248 57.0306 48.9555 54.872 48.2555 52.0354C47.6925 49.755 49.7548 47.6925 52.0354 48.2555C54.872 48.9555 57.0306 45.7248 55.2984 43.3722C53.9057 41.4804 55.0216 38.7861 57.3439 38.4335C60.2322 37.9946 60.9904 34.1839 58.4896 32.6729C56.479 31.4581 56.479 28.5419 58.4896 27.3271C60.9904 25.8163 60.2323 22.0055 57.3439 21.5665C55.0215 21.2134 53.9056 18.5196 55.2984 16.6278C57.0306 14.2752 54.872 11.0445 52.0354 11.7445C49.7549 12.3075 47.6925 10.2452 48.2555 7.9646C48.9555 5.12795 45.7248 2.96938 43.3722 4.70164C41.4804 6.09426 38.7861 4.9784 38.4335 2.65612C37.9946 -0.23224 34.1839 -0.990422 32.6729 1.51038C31.4581 3.52095 28.5419 3.52095 27.3271 1.51038C25.8163 -0.990382 22.0055 -0.232333 21.5665 2.65612C21.2134 4.97848 18.5196 6.09439 16.6278 4.70164C14.2752 2.96938 11.0445 5.12803 11.7445 7.9646C12.3075 10.245 10.2452 12.3074 7.9646 11.7445C5.12795 11.0445 2.96938 14.2752 4.70164 16.6278C6.09426 18.5196 4.9784 21.2139 2.65612 21.5665C-0.232239 22.0054 -0.990423 25.8161 1.51038 27.3271C3.52095 28.5419 3.52095 31.4581 1.51038 32.6729Z" />
                     </svg>
                   </div>
-                  <div className="flex items-center gap-2 -ml-11">
+                  <div className="flex items-center gap-2 -ml-11 z-40">
                     <p className=" uppercase text-white ">view all work</p>
                     <FaArrowRight className="text-[#027EEC]" />
                   </div>
@@ -63,51 +63,9 @@ function Hero() {
             </div>
           </div>
 
-          <div className=" space-y-4 -mt-28 cursor-default hidden lg:block">
+          <div className=" space-y-4 -mt-64 cursor-default hidden lg:block">
             <marquee direction="right">
-              <div className="text-[#1b1b1b] flex gap-3">
-                <p className="text-5xl font-bold lg:text-7xl">BRANDING</p>
-                <div className="mt-5 lg:mt-10">
-                  <GoNorthStar className="text-[#7654FD] text-2xl" />
-                </div>
-                <p className="text-5xl lg:text-7xl font-bold">
-                  WEB DEVELOPMENT
-                </p>
-                <div className="mt-5 lg:mt-10">
-                  <GoNorthStar className="text-[#7654FD] text-2xl" />
-                </div>
-                <p className="text-5xl lg:text-7xl font-bold">
-                  FRONT END DESIGN
-                </p>
-                <div className="mt-5 lg:mt-10">
-                  <GoNorthStar className="text-[#7654FD] text-2xl" />
-                </div>
-                <p className="text-5xl lg:text-7xl font-bold">WORDPRESS</p>
-                <div className="mt-5 lg:mt-10">
-                  <GoNorthStar className="text-[#7654FD] text-2xl" />
-                </div>
-                <p className="text-5xl lg:text-7xl font-bold">UXAUDIT</p>
-                <div className="mt-5 lg:mt-10">
-                  <GoNorthStar className="text-[#7654FD] text-2xl" />
-                </div>
-                <p className="text-5xl lg:text-7xl font-bold">
-                  MOBILE APP DEVELOPMENT
-                </p>
-                <div className="mt-5 lg:mt-10">
-                  <GoNorthStar className="text-[#7654FD] text-2xl" />
-                </div>
-                <p className="text-5xl lg:text-7xl font-bold">ECOMMERCE</p>
-                <div className="mt-5 lg:mt-10">
-                  <GoNorthStar className="text-[#7654FD] text-2xl" />
-                </div>
-                <p className="text-5xl lg:text-7xl font-bold">UI/UX DESIGN</p>
-                <div className="mt-5 lg:mt-10">
-                  <GoNorthStar className="text-[#7654FD] text-2xl" />
-                </div>
-              </div>
-            </marquee>
-            <marquee direction="">
-              <div className="text-[#1b1b1b] flex gap-3">
+              <div className="text-[#2C2C2C] flex gap-3">
                 <p className="text-5xl font-bold lg:text-7xl">BRANDING</p>
                 <div className="mt-5 lg:mt-10">
                   <GoNorthStar className="text-[#7654FD] text-2xl" />
@@ -151,7 +109,7 @@ function Hero() {
           </div>
           <div className="pt-16 lg:-mt-20">
             <div className="flex items-center justify-between">
-              <div className="lg:mt-20">
+              <div className="lg:mt-40">
                 <Link href={"#"} className="">
                   <div className="flex items-center gap-1">
                     <p className=" uppercase text-white font-semibold ">
@@ -163,7 +121,7 @@ function Hero() {
               </div>
               <div className=" lg:absolute video-area pl-64 md:pl-[40rem] lg:pl-0 lg:mt-20">
                 <div className="img_animation ">
-                  <Image src={img_ani} alt="amimation" className="" />
+                  <Image src={img_ani} alt="amimation" className="animate-spining" />
                 </div>
               </div>
             </div>
