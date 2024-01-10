66e0a873dc56d867e35f3d039584ccf3d6b8a8ee
FazlayRabbi <rabbisam74@gmail.com>
2024-01-10 21:21:38 +0600

    update code

--- app/page.js
+++ app/page.js
@@ -35,7 +35,7 @@ export default function Home() {
       <Lifecycle />
 
       <Recognized />
-      <ContactWithUs />
+      {/* <ContactWithUs /> */}
       <WhyChooseUs />
       <Learn />
       {/* <Research/> */}
